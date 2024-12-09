e9abf9c6eab3a1d34013e5a7d69e033ed067f60f
Felicia Favrholdt <feliciafavrholdt93@gmail.com>
2024-12-09 08:18:30 +0100

    minor styling changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,6 @@ import entranceImg from "../assets/images/entrance.png";
 
 const HomeContainer = styled.div`
   text-align: center;
-  padding: 20px;
 `;
 
 const ImagesContainer = styled.div`
@@ -19,16 +18,18 @@ const ImagesContainer = styled.div`
 const StyledImage = styled.img`
   width: 100%; /* Responsive width */
   max-width: 300px; /* Images will not exceed 300px */
-  height: auto; /* Maintain aspect ratio */ 
+  height: auto; /* Maintain aspect ratio */
 `;
 
 const Heading = styled.h1`
   font-size: 2.5rem;
-  color: #ff9505; 
+  color: #ff9505;
   margin-bottom: 10px;
 `;
 
 const Paragraph = styled.p`
+  width: 100%; /* Responsive width */
+  max-width: 800px; /* Limiting width for better readability */
   font-size: 1.2rem;
   color: #333;
   line-height: 1.6;
@@ -38,15 +39,15 @@ function Home() {
   return (
     <HomeContainer>
       <Heading>Welcome to VetLocator</Heading>
+      <Paragraph>
+        Find veterinarians in your area, available for emergencies and general
+        consultations. Designed to support pet owners in urgent situations.
+      </Paragraph>
       <ImagesContainer>
         <StyledImage src={homeImg} alt="Home" />
         <StyledImage src={dogImg} alt="Dog" />
         <StyledImage src={entranceImg} alt="Entrance" />
       </ImagesContainer>
-      <Paragraph>
-        Find veterinarians in your area, available for emergencies and general
-        consultations. Designed to support pet owners in urgent situations.
-      </Paragraph>
     </HomeContainer>
   );
 }
